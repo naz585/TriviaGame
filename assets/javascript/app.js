671571d2d4b8d96d8e9a5d7e58b1eac73c2d280d
naz585 <naz585@aol.com>
2018-07-23 12:49:24 -0400

    css additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,84 @@
 
 
 var question1 = {
-     Q : "How much blood does the Average Person have?",
-     A1 : "9 pints",
-     A2 : "10 pints",
-     A3 : "11 pints",
-     A4 : "12 pints" //answer
+    Q: "How much blood does the Average Person have?",
+    A1: "9 pints",
+    A2: "10 pints",
+    A3: "11 pints",
+    A4: "12 pints" //answer
 }
 
 var question2 = {
-    Q : "How many teeth does the average Person have?",
-    A1 : "30",
-    A2 : "29",
-    A3 : "32",  //answer
-    A4 : "28"
+    Q: "How many teeth does the average Person have?",
+    A1: "30",
+    A2: "29",
+    A3: "32",  //answer
+    A4: "28"
 }
 
 var question3 = {
-    Q : "How long can an average human go without food?",
-    A1 : "1 week",
-    A2 : "5 weeks",
-    A3 : "2 weeks",
-    A4 : "3 weeks" //answer
+    Q: "How long can an average human go without food?",
+    A1: "1 week",
+    A2: "5 weeks",
+    A3: "2 weeks",
+    A4: "3 weeks" //answer
 }
 
 var question4 = {
-    Q : "Which is the most important organ of your body?",
-    A1 : "Heart",
-    A2 : "Lungs",
-    A3 : "Brain", //answer
-    A4 : "Stomach"
+    Q: "Which is the most important organ of your body?",
+    A1: "Heart",
+    A2: "Lungs",
+    A3: "Brain", //answer
+    A4: "Stomach"
 }
 
 var question5 = {
-    Q : "How many bones are in the rib cage?",
-    A1 : "12",
-    A2 : "24", //answer
-    A3 : "13",
-    A4 : "26"
+    Q: "How many bones are in the rib cage?",
+    A1: "12",
+    A2: "24", //answer
+    A3: "13",
+    A4: "26"
 }
 
 var question6 = {
-    Q : "What is the weight of the average human brain?",
-    A1 : "2.6 pounds",
-    A2 : "2.7 pounds", //answer
-    A3 : "3.1 pounds",
-    A4 : "2.5 pounds"
+    Q: "What is the weight of the average human brain?",
+    A1: "2.6 pounds",
+    A2: "2.7 pounds", //answer
+    A3: "3.1 pounds",
+    A4: "2.5 pounds"
 }
 
 
 var question7 = {
-    Q : "How fast can the average human run?",
-    A1 : "30 mph",
-    A2 : "29 mph",
-    A3 : "27 mph",
-    A4 : "28 mph" //answer
+    Q: "How fast can the average human run?",
+    A1: "30 mph",
+    A2: "29 mph",
+    A3: "27 mph",
+    A4: "28 mph" //answer
 }
 
 var question8 = {
-    Q : "What is the average human body temperature",
-    A1 : "98.6", //answer
-    A2 : "96.8",
-    A3 : "96.4",
-    A4 : "98.4"
+    Q: "What is the average human body temperature",
+    A1: "98.6", //answer
+    A2: "96.8",
+    A3: "96.4",
+    A4: "98.4"
 }
 
 var question9 = {
-    Q : "Where is the strongest muscle in the human body",
-    A1 : "Brain", 
-    A2 : "Arms",
-    A3 : "Jaw", //answer
-    A4 : "Legs"
+    Q: "Where is the strongest muscle in the human body",
+    A1: "Brain",
+    A2: "Arms",
+    A3: "Jaw", //answer
+    A4: "Legs"
 }
 
 var question10 = {
-    Q : "What is the largest organ in the human body",
-    A1 : "Heart", 
-    A2 : "Stomach",
-    A3 : "skin", //answer
-    A4 : "Brain"
+    Q: "What is the largest organ in the human body",
+    A1: "Heart",
+    A2: "Stomach",
+    A3: "skin", //answer
+    A4: "Brain"
 }
 
 $(".question-1").text(question1.Q);
@@ -103,128 +103,144 @@ $("#2A4").attr("value", question2.A4);
 
 $(".question-3").text(question3.Q);
 $(".3A1").append(question3.A1)
-$("#3A1").attr("value",question3.A1);
+$("#3A1").attr("value", question3.A1);
 $(".3A2").append(question3.A2)
-$("#3A2").attr("value",question3.A2);
+$("#3A2").attr("value", question3.A2);
 $(".3A3").append(question3.A3)
-$("#3A3").attr("value",question3.A3);
+$("#3A3").attr("value", question3.A3);
 $(".3A4").append(question3.A4)
-$("#3A4").attr("value",question3.A4);
+$("#3A4").attr("value", question3.A4);
 
 $(".question-4").text(question4.Q);
 $(".4A1").append(question4.A1)
-$("#4A1").attr("value",question4.A1);
+$("#4A1").attr("value", question4.A1);
 $(".4A2").append(question4.A2)
-$("#4A2").attr("value",question4.A2);
+$("#4A2").attr("value", question4.A2);
 $(".4A3").append(question4.A3)
-$("#4A3").attr("value",question4.A3);
+$("#4A3").attr("value", question4.A3);
 $(".4A4").append(question4.A4)
-$("#4A4").attr("value",question4.A4);
+$("#4A4").attr("value", question4.A4);
 
 $(".question-5").text(question5.Q);
 $(".5A1").append(question5.A1)
-$("#5A1").attr("value",question5.A1);
+$("#5A1").attr("value", question5.A1);
 $(".5A2").append(question5.A2)
-$("#5A2").attr("value",question5.A2);
+$("#5A2").attr("value", question5.A2);
 $(".5A3").append(question5.A3)
-$("#5A3").attr("value",question5.A3);
+$("#5A3").attr("value", question5.A3);
 $(".5A4").append(question5.A4)
-$("#5A4").attr("value",question5.A4);
+$("#5A4").attr("value", question5.A4);
 
 $(".question-6").text(question6.Q);
 $(".6A1").append(question6.A1)
-$("#6A1").attr("value",question6.A1);
+$("#6A1").attr("value", question6.A1);
 $(".6A2").append(question6.A2)
-$("#6A2").attr("value",question6.A2);
+$("#6A2").attr("value", question6.A2);
 $(".6A3").append(question6.A3)
-$("#6A3").attr("value",question6.A3);
+$("#6A3").attr("value", question6.A3);
 $(".6A4").append(question6.A4)
-$("#6A4").attr("value",question6.A4);
+$("#6A4").attr("value", question6.A4);
 
 $(".question-7").text(question7.Q);
 $(".7A1").append(question7.A1)
-$("#7A1").attr("value",question7.A1);
+$("#7A1").attr("value", question7.A1);
 $(".7A2").append(question7.A2)
-$("#7A2").attr("value",question7.A2);
+$("#7A2").attr("value", question7.A2);
 $(".7A3").append(question7.A3)
-$("#7A3").attr("value",question7.A3);
+$("#7A3").attr("value", question7.A3);
 $(".7A4").append(question7.A4)
-$("#7A4").attr("value",question7.A4);
+$("#7A4").attr("value", question7.A4);
 
 $(".question-8").text(question8.Q);
 $(".8A1").append(question8.A1)
-$("#8A1").attr("value",question8.A1);
+$("#8A1").attr("value", question8.A1);
 $(".8A2").append(question8.A2)
-$("#8A2").attr("value",question8.A2);
+$("#8A2").attr("value", question8.A2);
 $(".8A3").append(question8.A3)
-$("#8A3").attr("value",question8.A3);
+$("#8A3").attr("value", question8.A3);
 $(".8A4").append(question8.A4)
-$("#8A4").attr("value",question8.A4);
+$("#8A4").attr("value", question8.A4);
 
 $(".question-9").text(question9.Q);
 $(".9A1").append(question9.A1)
-$("#9A1").attr("value",question9.A1);
+$("#9A1").attr("value", question9.A1);
 $(".9A2").append(question9.A2)
-$("#9A2").attr("value",question9.A2);
+$("#9A2").attr("value", question9.A2);
 $(".9A3").append(question9.A3)
-$("#9A3").attr("value",question9.A3);
+$("#9A3").attr("value", question9.A3);
 $(".9A4").append(question9.A4)
-$("#9A4").attr("value",question9.A4);
+$("#9A4").attr("value", question9.A4);
 
 $(".question-10").text(question10.Q);
 $(".10A1").append(question10.A1)
-$("#10A1").attr("value",question10.A1);
+$("#10A1").attr("value", question10.A1);
 $(".10A2").append(question10.A2)
-$("#10A2").attr("value",question10.A2);
+$("#10A2").attr("value", question10.A2);
 $(".10A3").append(question10.A3)
-$("#10A3").attr("value",question10.A3);
+$("#10A3").attr("value", question10.A3);
 $(".10A4").append(question10.A4)
-$("#10A4").attr("value",question10.A4);
+$("#10A4").attr("value", question10.A4);
 
+var audio = new Audio('assets/javascript/saw.mp3');
+audio.play();
 
-var time = 60;
-$(".timer").text(time)
-var x = setInterval(function() {
+var time = 120;
 
-    // Get todays date and time
-  
-    // Find the distance between now an the count down date
-    
-  
-    // Time calculations for days, hours, minutes and seconds
+var x = setInterval(function () {
+
     time -= 1;
-  
-    // Display the result in the element with id="demo"
-    $(".timer").text(time)
-  
-    // If the count down is finished, write some text 
+    $(".timer").text(timeConverter(time))
+
+
+
+
     if (time < 0) {
-      clearInterval(x);
-      $(".trivia").hide();
-      $(".timer").text("Game Over");
-      $("h1").text(" Your Score ");
-      $("h2").text("10/"+correct);
+        clearInterval(x);
+        $(".trivia").hide();
+        $(".timer").text("Game Over");
+        $("h1").text(" Your Score ");
+        $("h2").text("10/" + correct);
     }
-  }, 1000);
+}, 1000);
+
+$(".timer").text(timeConverter(time))
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
 
 
 function getRadioVal(form, name) {
     var val;
-    // get list of radio buttons with specified name
+
     var radios = form[0].elements[name];
-    
-    // loop through list of radio buttons
-    for (var i=0, len=radios.length; i<len; i++) {
-        if ( radios[i].checked ) { // radio checked?
-        val = radios[i].value; // if so, hold its value in val
-        break; // and break out of for loop
+
+
+    for (var i = 0, len = radios.length; i < len; i++) {
+        if (radios[i].checked) {
+            val = radios[i].value;
+            break;
         }
     }
-    return val; // return value of checked radio or undefined if none checked
+    return val;
 }
 var correct = 0;
 
-$("button").click(function(){
+$("button").click(function () {
     var Q1 = getRadioVal($(".Q1"), "optradio");
     var Q2 = getRadioVal($(".Q2"), "optradio");
     var Q3 = getRadioVal($(".Q3"), "optradio");
@@ -236,45 +252,45 @@ $("button").click(function(){
     var Q9 = getRadioVal($(".Q9"), "optradio");
     var Q10 = getRadioVal($(".Q10"), "optradio");
 
-    if (Q1 === question1.A4) { 
-        correct+=1;
-    } 
+    if (Q1 === question1.A4) {
+        correct += 1;
+    }
     if (Q2 === question2.A3) {
-        correct+=1;
+        correct += 1;
 
-    }  
+    }
     if (Q3 === question3.A4) {
-        correct+=1;
-    } 
-    if (Q4 === question4.A3) { 
-        correct+=1;
-    } 
-    if (Q5 === question5.A2) {
-        correct+=1;
-    } 
-    if (Q6 === question6.A2) {
-        correct+=1;
-    } 
-    if (Q7 === question7.A4) {
-        correct+=1;
-    } 
-    if (Q8 === question8.A1) {
-        correct+=1;
-    } 
-    if (Q9 === question9.A3) {
-        correct+=1;
+        correct += 1;
     }
-    
-    if (Q10 === question10.A3){
-        correct+=1;
+    if (Q4 === question4.A3) {
+        correct += 1;
+    }
+    if (Q5 === question5.A2) {
+        correct += 1;
+    }
+    if (Q6 === question6.A2) {
+        correct += 1;
+    }
+    if (Q7 === question7.A4) {
+        correct += 1;
+    }
+    if (Q8 === question8.A1) {
+        correct += 1;
+    }
+    if (Q9 === question9.A3) {
+        correct += 1;
     }
 
-    
+    if (Q10 === question10.A3) {
+        correct += 1;
+    }
+
+
     $(".trivia").hide();
-    
-    console.log("10/"+correct);
+
+    console.log(correct + "/10");
     $("h1").text(" Your Score ");
-    $("h2").text("10/"+correct);
+    $("h2").text(correct + "/10");
 
 });
 
